17ff01ea4bbcf81cf4333618a223e5caa9dfcaa2
Alexander Schmidt <anschluss80+git@mailbox.org>
2017-05-07 15:51:05 +0200

    Make logger optional in AbstractService

--- src/Service/AbstractService.php
+++ src/Service/AbstractService.php
@@ -17,11 +17,11 @@ abstract class AbstractService
     /**
      * Constructor
      *
-     * @param   LogHelper   $logger     Logging helper
+     * @param   LogHelper   $logger     optional Logging helper
      * @access  public
      * @author  a.schmidt@internet-of-voice.de
      */
-    public function __construct(LogHelper $logger) {
+    public function __construct(LogHelper $logger = null) {
         $this->logger = $logger;
     }
 
