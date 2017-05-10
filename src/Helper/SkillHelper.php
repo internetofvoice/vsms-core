1ac76479011db0f785db930daafb131129622d37
Alexander Schmidt <anschluss80+git@mailbox.org>
2017-05-10 23:07:58 +0200

    Add SkillHelper Skeleton

---
+++ src/Helper/SkillHelper.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace InternetOfVoice\VSMS\Core\Helper;
+
+/**
+ * SkillHelper
+ *
+ * @author  Alexander Schmidt <a.schmidt@internet-of-voice.de>
+ */
+class SkillHelper
+{
+    /**
+     * extractAmazonDate
+     *
+     * Extracts AMAZON.DATE slot values to DateTime AND DateInterval objects.
+     * Returns an StdClass object with properties 'start' (DateTime object) and 'duration' (DateInterval object).
+     *
+     * If slot value contain a time span like one year, 'start' will hold the first day at zero time,
+     * and 'period' an interval of one year.
+     *
+     * If a specific date like 2017-05-10 is given, 'start is 2017-05-10 at zero time, 'period' is one day.
+     *
+     * @param 	string      $amazon_date        AMAZON.DATE slot value
+     * @return  \StdClass
+     * @access	public
+     * @author	a.schmidt@internet-of-voice.de
+     */
+    public function extractAmazonDate($amazon_date) {
+        $start  = false;
+        $period = false;
+
+        return (object)[$start, $period];
+    }
+}
